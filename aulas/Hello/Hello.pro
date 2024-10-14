3f68e09cd17373be4f6e53914c9782d92d3dfb1a
Hugo Marcondes <hugo.marcondes@ifsc.edu.br>
2024-10-14 17:46:40 -0300

    Refatorando para o uso de tags para controle de versões dos exemplos de aula

--- aulas/Hello/Hello.pro
+++ aulas/Hello/Hello.pro
@@ -4,4 +4,8 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
-        main.cpp
+        main.cpp \
+        world.cpp
+
+HEADERS += \
+    world.h
--- aulas/Hello/main.cpp
+++ aulas/Hello/main.cpp
@@ -1,11 +1,44 @@
+#include "world.h"
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    cout << "Hello PRG203405!!\n";
-    cout << "Goodbye!";
+World meu_mundo("World");
+
+void functionHello(){
+    cout << "Entering functionHello()" << endl;
+    World obj("FunctionHello");
+    obj.sayHi();
+    cout << "Leaving functionHello()" << endl;
+}
+
+
+int main() {
+    cout << "Entering main" << endl;
+    World objeto("MainWorld");
+    objeto.sayHi();
+    cout << "obj name =" << objeto.getName() << endl;
+
+    cout << "Calling functionHello()" << endl;
+    functionHello();
+    cout << "Returning functionHello()" << endl;
+
+    World * ptr_obj;
+    ptr_obj = new World("WorldPtr");
+    ptr_obj->sayHi();
+    delete ptr_obj;
+
+    /*
+    objeto.setName("MarioWorld");
+    objeto.sayHi();
+
+    objeto.setName("Mordor");
+    objeto.sayHi();
+
+    objeto.setName("");
+    objeto.sayHi();
+    */
+
+    cout << "Leaving main" << endl;
     return 0;
 }
