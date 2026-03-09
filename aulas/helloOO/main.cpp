d29c5e577a627264c6c864521497c3e54555d917
Hugo Marcondes <hugo.marcondes@ifsc.edu.br>
2026-03-09 20:45:37 -0300

    Aula 09/03/2026

--- aulas/helloOO/main.cpp
+++ aulas/helloOO/main.cpp
@@ -3,32 +3,92 @@
 
 namespace Hugo {
 	int variavel = 5;
+
+	namespace Config {
+		int variavel = 15;
+	};
+
 };
 
 namespace ifsc {
 	int variavel = 10;
-}
+};
 
 using namespace std;
 using namespace Hugo;
 
 class World {
 
+private:
+	std::string _name;
+	void sayIt(string value);
+
 public:
-	void sayHello() {
-		cout << "Hello helloOO !" << endl;
+	World();
+	World(string value);
+
+	void name(string value) {
+		_name = value;
 	}
+	string name() {
+		return _name;
+	}
+	void sayHello();
+	void sayGoodbye();
+	void sayHi();
 
 };
 
+World::World(){
+	_name = "no_name";
+}
+
+World::World(string value){
+	_name = value;
+}
+
+void World::sayIt(string value) {
+	cout << value << " " << _name << " !" << endl;
+}
+
+void World::sayHello() {
+		sayIt("Hello");
+}
+void World::sayGoodbye() {
+		sayIt("Goodbye");
+}
+void World::sayHi() {
+		sayIt("Hi");
+}
+
+
 int main (int argc, char* argv[]) {
 
+	cout << "Primeiro exemplo de código C++" << endl;
 
 	World object;
+	World obj1("mundo01");
+	World obj2("mundo02"); 
+
+	//object.setName("Hugo");
+
+	object.name("Hugo");
+	cout << object.name();
 
 	object.sayHello();
+	obj1.sayHello();
+	obj2.sayHello();
 
 
-	std::cout << "O valor da variável é = " << variavel << std::endl;
+
+	string fname = "Hugo";
+	string lname = "Marcondes";
+
+	string fullName = fname + " " + lname;
+
+	cout << "Full name = " << fullName << endl;
+
+	std::cout << "O valor da variável é = " << Hugo::Config::variavel << std::endl;
+
 	return 0;
 }
